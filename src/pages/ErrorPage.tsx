ebec65f71f668b286595e94e3917b82c0f1c6199
miruashvili111 <113052274+miruashvili111@users.noreply.github.com>
2025-09-26 02:16:32 +0400

    add error boundary

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,7 +1,33 @@
+import { Box, Button, Typography } from "@mui/material"
 
 const ErrorPage = () => {
     return (
-        <div>ErrorPage</div>
+        <Box
+            sx={{
+                minHeight: 'calc(100vh - 60px)',
+                maxWidth: 1920,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                textAlign: 'center'
+            }}
+        >
+            <Typography variant="h2" color="textPrimary" gutterBottom>
+                Something Went Wrong
+            </Typography>
+            <Typography variant="h6" color="textSecondary">
+                An unexpected error occurred. Please try going back to the home page or reload the page
+            </Typography>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={() => window.location.reload()}
+                sx={{ mt: 5 }}
+            >
+                Reload Page
+            </Button>
+        </Box>
     )
 }
 
